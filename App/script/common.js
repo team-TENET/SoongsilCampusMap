42c3002393d263aff3b2fc0e9046fd5d5401ce93
tlfzgjadyd <tlfzgjadyd@naver.com>
2021-05-11 20:08:53 +0900

    홈화면 및 기본 페이지 이동 완성

---
+++ App/script/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#homeIcon').click(function(){
+        window.location.href="./home.html";
+    })
+    $('#mapIcon').click(function(){
+        window.location.href="./map.html";
+    })
+    $('#infoBoardIcon').click(function(){
+        window.location.href="./infoBoard.html";
+    })
+    $('#freeBoardIcon').click(function(){
+        window.location.href="./freeBoard.html";
+    })
+})
